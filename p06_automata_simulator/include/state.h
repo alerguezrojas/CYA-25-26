b5c70049c5bad4c068109a974ef0b59608be5f3f
Alejandro Rodríguez Rojas <alu0101317038@ull.edu.es>
2025-10-19 17:25:19 +0000

    añadida implementación de la clase State en state.cc y definición en state.h

---
+++ p06_automata_simulator/include/state.h
@@ -0,0 +1,47 @@
+// Universidad de La Laguna
+// Escuela Superior de Ingeniería y Tecnología
+// Grado en Ingeniería Informática
+// Asignatura: Computabilidad y Algoritmia
+// Curso: 2°
+// Práctica 6: Diseño e implementación de un simulador de autómatas finitos
+// Autor: Alejandro Rodríguez Rojas
+// Correro: alu0101317038@ull.edu.es
+// Fecha de entrega: 21/10/2025
+// Archivo state.h: Fichero que contiene la definición de la clase Estado
+// Referencias:
+// Historial de revisiones
+//   19/10/2025 - Creacion del codigo version 1.0
+
+#ifndef STATE_H
+#define STATE_H
+
+#include <set>
+#include <string>
+
+#include "symbol.h"
+
+class Transition;
+
+class State {
+ public:
+  State();
+  State(const std::string& state_id, bool start_state, bool accept_state);
+  State(const std::string& state_id, bool start_state, bool accept_state,
+        std::set<Transition> transitions);
+  ~State() = default;
+  bool IsStartState() const;
+  bool IsAcceptState() const;
+  std::string GetStateId() const;
+  std::set<Transition> GetTransitions(Symbol symbol) const;
+  bool operator<(const State& other) const;
+  bool operator==(const State& other) const;
+
+ private:
+  std::string state_id_;
+  bool start_state_;
+  bool accept_state_;
+  int transitions_number_;
+  std::set<Transition> transitions_;
+};
+
+#endif  // STATE_H
---
+++ p06_automata_simulator/src/state.cc
@@ -0,0 +1,62 @@
+// Universidad de La Laguna
+// Escuela Superior de Ingeniería y Tecnología
+// Grado en Ingeniería Informática
+// Asignatura: Computabilidad y Algoritmia
+// Curso: 2°
+// Práctica 6: Diseño e implementación de un simulador de autómatas finitos
+// Autor: Alejandro Rodríguez Rojas
+// Correro: alu0101317038@ull.edu.es
+// Fecha de entrega: 21/10/2025
+// Archivo state.cc: Fichero que contiene la implementación de la clase Estado
+// Referencias:
+// Historial de revisiones
+//   19/10/2025 - Creacion del codigo version 1.0
+
+#include "state.h"
+
+State::State() {
+  state_id_ = "";
+  start_state_ = false;
+  accept_state_ = false;
+  transitions_number_ = 0;
+}
+
+State::State(const std::string& state_id, bool start_state, bool accept_state) {
+  state_id_ = state_id;
+  start_state_ = start_state;
+  accept_state_ = accept_state;
+  transitions_number_ = 0;
+}
+
+State::State(const std::string& state_id, bool start_state, bool accept_state,
+             std::set<Transition> transitions) {
+  state_id_ = state_id;
+  start_state_ = start_state;
+  accept_state_ = accept_state;
+  transitions_ = transitions;
+  transitions_number_ = transitions.size();
+}
+
+bool State::IsStartState() const {
+  return start_state_;
+}
+
+bool State::IsAcceptState() const {
+  return accept_state_;
+}
+
+std::string State::GetStateId() const {
+  return state_id_;
+}
+
+std::set<Transition> State::GetTransitions(Symbol symbol) const {
+  
+}
+
+bool State::operator<(const State& other) const {
+  return state_id_ < other.state_id_;
+}
+
+bool State::operator==(const State& other) const {
+  return state_id_ == other.state_id_;
+}
