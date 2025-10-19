2162fa59cae980568334da4f9eac7a51527c1901
Alejandro Rodríguez Rojas <alu0101317038@ull.edu.es>
2025-10-19 19:34:27 +0000

    añadida implementación de la clase Language en language.cc y definición en language.h

---
+++ p06_automata_simulator/src/language.cc
@@ -0,0 +1,68 @@
+// Universidad de La Laguna
+// Escuela Superior de Ingeniería y Tecnología
+// Grado en Ingeniería Informática
+// Asignatura: Computabilidad y Algoritmia
+// Curso: 2°
+// Práctica 6: Diseño e implementación de un simulador de autómatas finitos
+// Autor: Alejandro Rodríguez Rojas
+// Correro: alu0101317038@ull.edu.es
+// Fecha de entrega: 21/10/2025
+// Archivo language.cc: Fichero que contiene la implementación de la clase Language
+// Referencias:
+// Historial de revisiones
+//   20/09/2025 - Creacion del codigo version 1.0
+
+#include "language.h"
+
+/**
+ * @brief Default constructor for Language class.
+ */
+Language::Language() {}
+
+/**
+ * @brief Constructor for Language class that initializes the language with a set of chains.
+ * @param language The set of chains to initialize the language with.
+ */
+Language::Language(const std::set<Chain>& language) {
+  language_ = language;
+}
+
+/**
+ * @brief Getter for the language set.
+ * @return The set of chains representing the language.
+ */
+std::set<Chain> Language::GetLanguage() const {
+  return language_;
+}
+
+/**
+ * @brief Setter for the language set.
+ * @param language The set of chains to set as the language.
+ */
+void Language::SetLanguage(const std::set<Chain>& language) {
+  language_ = language;
+}
+
+/**
+ * @brief Output stream operator for printing the language.
+ * @param os The output stream.
+ * @param language The language to print.
+ * @return The output stream with the language appended.
+ */
+std::ostream& operator<<(std::ostream& os, const Language& language) {
+  os << "{";
+  if (language.language_.empty()) {
+    os << "}";
+    return os;
+  }
+  bool first = true;
+  for(const auto& chain : language.language_) {
+    if (!first) {
+      os << ", ";
+    }
+    os << chain;
+    first = false;
+  }
+  os << "}";
+  return os;
+}
