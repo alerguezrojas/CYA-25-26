7335d3a337afc0d78ec13d5d6b7b8432379b5d56
Alejandro Rodríguez Rojas <alu0101317038@ull.edu.es>
2025-12-08 17:29:02 +0000

    p10 A tiempo de ejecucion (tutorizada sin nota)

---
+++ p10A_execution_time/time/build/sorting/CMakeFiles/sorting_library.dir/src/merge_sort.cpp.o.d
@@ -0,0 +1,17 @@
+sorting/CMakeFiles/sorting_library.dir/src/merge_sort.cpp.o: \
+ /home/usuario/CYA-25-26/time/sorting/src/merge_sort.cpp \
+ /usr/include/stdc-predef.h \
+ /home/usuario/CYA-25-26/time/sorting/include/merge_sort.hpp \
+ /usr/include/c++/13/cstddef \
+ /usr/include/x86_64-linux-gnu/c++/13/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/13/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/13/bits/cpu_defines.h \
+ /usr/include/c++/13/pstl/pstl_config.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h
